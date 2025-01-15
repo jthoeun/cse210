16cc882045b3ff10a76b46dfb300cb3b4d0851d1
jthoeun <johnny.thoeun@gmail.com>
2025-01-14 20:22:19 -0700

    Prep 5 commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string username = PromptUserName();
+        int usernumber = PromptUserNumber();
+        int squarenumber = squareNumber(usernumber);
+        DisplayResult(username, squarenumber);
+
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
