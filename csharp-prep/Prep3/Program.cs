25bad55eb1f24547503e1876948f9b0537073904
jthoeun <johnny.thoeun@gmail.com>
2025-01-14 19:46:54 -0700

    Prep 3 Assignement Commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    //Console.Write("What is the magic number?");
+    //int magicNumber = int.Parse(Console.ReadLine());
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnumberGenereator = new Random();
+        int magicNumber = randomnumberGenereator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
