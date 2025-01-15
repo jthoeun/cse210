7483c1648e1dcafe81377b89fe9b9b61138da063
jthoeun <johnny.thoeun@gmail.com>
2025-01-14 20:03:26 -0700

    Prep 4 commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int inputNumber = -1;
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter a number (type 0 when finished): ");
+            string userResponse = Console.ReadLine();
+            inputNumber = int.Parse(userResponse);
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float avereage = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {avereage}");
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is {max}");
     }
 }
