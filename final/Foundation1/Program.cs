20dbf412987b2784f4709128cff5c4e5a8cf4f3e
jthoeun <johnny.thoeun@gmail.com>
2025-03-27 19:48:37 -0600

    Foundation 1 Inital Commit

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video = new Video("Learning C#", "John Doe", 300);
+        video.AddComment(new Comment("Alice", "Great video!"));
+        video.AddComment(new Comment("Bob", "Very helpful, thanks!"));
+        video.AddComment(new Comment("Charlie", "I learned a lot."));
+        video.DisplayVideoInfo();
     }
 }
